7f8e2d741e6c6364f2becdb8dd1b14695a5fa2d6
neocdtv <krzysztof.wolf.1980@gmail.com>
2018-05-12 11:55:35 +0200

    add src

---
+++ src/main/java/io/neocdtv/App.java
@@ -0,0 +1,13 @@
+package io.neocdtv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
